11411411c83f6958b8d8c6ffee4a37b6664eaa13
dimmalaAlekya16 <143582987+dimmalaAlekya16@users.noreply.github.com>
2023-10-14 10:12:29 +0530

    import java.util.*; public class capacity {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         int s=sc.nextInt();         int b=sc.nextInt();         int c=(2*t*s*b*512);         int k=c/1024;         System.out.printf("%d KB",k);     } }

---
+++ Capacity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class capacity
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int s=sc.nextInt();
+        int b=sc.nextInt();
+        int c=(2*t*s*b*512);
+        int k=c/1024;
+        System.out.printf("%d KB",k);
+    }
+}
